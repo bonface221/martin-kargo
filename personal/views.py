2c85ad6547ea5fe2eab847fa10dc69cab036ed91
Dean Kago <martokhago@gmail.com>
2022-05-29 23:13:30 +0300

    fetched all categories

--- personal/views.py
+++ personal/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
+  category = Category.objects.all()
   return render(request, 'main/index.html', locals())
+
+def categorySlide(request):
+  return render(request, 'main/', locals())
