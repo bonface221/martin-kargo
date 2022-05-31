7fa4beaf0c4b089a68bf97fb66bd01260ee2d892
Dean Kago <martokhago@gmail.com>
2022-05-31 21:10:44 +0300

    initial heroku push

--- gallery/settings.py
+++ gallery/settings.py
@@ -146,7 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
