7b93dbf48baf9b669189501f6776abc0e919c0fd
Dean Kago <martokhago@gmail.com>
2022-05-31 14:00:25 +0300

    polished on copy image link

--- personal/static/assets/js/main.js
+++ personal/static/assets/js/main.js
@@ -1,14 +1,9 @@
-console.log("Hello world!")
-
 const copyBtns =[...document.getElementsByClassName('copy-btn')]
-console.log(copyBtns)
 
 let previous = null
 
 copyBtns.forEach(btn=> btn.addEventListener('click', ()=>{
-  console.log('click')
   const image = btn.getAttribute('data-landImage.url')
-  console.log(image)
   navigator.clipboard.writeText(image)
   btn.textContent = 'copied'
 
@@ -16,5 +11,5 @@ copyBtns.forEach(btn=> btn.addEventListener('click', ()=>{
     previous.textContent = 'Copy Link'
   }
   previous = btn
-  
+
 }))
