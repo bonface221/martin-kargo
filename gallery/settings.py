3af5e6d619b2d88f68543f7f0d3f2332d2a3c7ea
martin <martokhago@gmail.com>
2023-07-13 22:34:25 +0300

    railway.toml comment

--- gallery/settings.py
+++ gallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-MODE=config("MODE", default="dev")
+MODE=config("MODE", default="")
 DATABASE_URL = 'postgresql://postgres:buTLv7NQzEMER4Ly4upL@containers-us-west-149.railway.app:7955/railway'
 
 
