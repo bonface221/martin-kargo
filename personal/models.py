c30f0be3bb3bad54af2250144110955d15dd75de
bonface221 <mainaboniface221@gmail.com>
2023-07-17 12:50:43 +0300

    final-changes

--- personal/models.py
+++ personal/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils import timezone
-from django_resized import ResizedImageField
+from cloudinary.models import CloudinaryField
 from django.template.defaultfilters import slugify
 from uuid import uuid4
 from django.urls import reverse
-import cloudinary
+
 # Create your models here.
 
 
@@ -40,12 +40,10 @@ class Image(models.Model):
     description = models.TextField(null=True, blank=True)
     text = models.TextField(null=True, blank=True)
 
-    squareImage = cloudinary.CloudinaryImage(
-        'default_square.jpg').build_url(width=1000, height=1000, crop="fill")
-    landImage = cloudinary.CloudinaryImage('default_land.jpg').build_url(
-        width=2878, height=1618, crop="fill")
-    tallImage = cloudinary.CloudinaryImage('default_tall.jpg').build_url(
-        width=1618, height=2878, crop="fill")
+    squareImage = CloudinaryField('squareImage', default=''
+                                  )
+    landImage = CloudinaryField('landImage', default='')
+    tallImage = CloudinaryField('tallImage', default='')
 
     category = models.ForeignKey(
         Category, null=True, blank=True, on_delete=models.CASCADE)
@@ -53,26 +51,14 @@ class Image(models.Model):
     # utility variable
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
     slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    last_updated = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_created=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} {}'.format(self.category.title, self.uniqueId)
 
     def get_absolute_url(self):
         return reverse('image-detail', kwargs={'slug': self.slug})
-
-    def save(self, *args, **kwargs):
-        if self.date_created is None:
-            self.date_created = timezone.localtime(timezone.now())
-        if self.uniqueId is None:
-            self.uniqueId = str(uuid4()).split('-')[4]
-            self.slug = slugify('{} {}'.format(
-                self.category.title, self.uniqueId))
-
-        self.slug = slugify('{} {}'.format(self.category.title, self.uniqueId))
-        self.last_updated = timezone.localtime(timezone.now())
-        super(Image, self).save(*args, **kwargs)
 
     def upgrade(self):
         self.delete()
